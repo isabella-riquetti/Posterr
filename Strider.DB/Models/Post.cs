8ebeb7d9da500df334da5c4dfdf6d86bc4485d93
Isabella Riquetti <riquetti.isabella@gmail.com>
2022-04-18 15:24:15 -0300

    Add project files.

---
+++ Strider.DB/Models/Post.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Strider.DB.Models
+{
+    internal class Post
+    {
+        public int Id { get; set; }
+        public User User { get; set; }
+        public string Content { get; set; }
+        public IList<User> Retweets { get; set; }
+    }
+}
---
+++ Strider.DB/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strider.DB.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Username { get; set; }
+
+        public IList<Post> Posts { get; set; }
+    }
+}
